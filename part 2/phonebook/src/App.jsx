e90c0e6925f565873edf87c65d410fa9d24e4ff4
chaitanya02082000 <chaitanyagithub0208@gmail.com>
2025-08-18 20:33:45 +0530

    did 2.9

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -3,21 +3,27 @@ const Entry = ({ number, name }) => {
   return (
     <>
       <p>
-        {" "}
-        {name}
-        {number}
+        {name} {number}
       </p>
     </>
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [filter, setFilter] = useState("");
   const [newNumber, setNewNumber] = useState([]);
   const onChangeName = (event) => {
     setNewName(event.target.value);
   };
-
+  const onChangeFilter = (event) => {
+    setFilter(event.target.value);
+  };
   const onChangeNumber = (event) => {
     setNewNumber(event.target.value);
   };
@@ -47,9 +53,13 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter show with a{" "}
+      <input value={filter} onChange={onChangeFilter}></input>
       <form onSubmit={handleAdd}>
         <div>
+          <h2>add a new: </h2>
           name: <input value={newName} onChange={onChangeName} />
+          <br></br>
           Numbers: <input value={newNumber} onChange={onChangeNumber} />
         </div>
         <div>
@@ -57,11 +67,28 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((item, key) => (
-        <Entry key={key} name={item.name} number={item.number}></Entry>
-      ))}
+      {filter ? (
+        <ul>
+          {persons
+            .filter((item) =>
+              item.name.toLowerCase().includes(filter.toLowerCase()),
+            )
+            .map((item, index) => (
+              <li key={index}>
+                <Entry name={item.name} number={item.number} />
+              </li>
+            ))}
+        </ul>
+      ) : (
+        <ul>
+          {persons.map((item, index) => (
+            <li key={index}>
+              <Entry name={item.name} number={item.number} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
-
 export default App;
