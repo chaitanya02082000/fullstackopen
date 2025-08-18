374d29361ae7368073b11313a0dfffb297198fd9
chaitanya02082000 <chaitanyagithub0208@gmail.com>
2025-08-18 22:35:22 +0530

    did 2.11

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const Persons = ({ number, name }) => {
   return (
     <>
@@ -57,12 +58,7 @@ const AddPeople = ({
   );
 };
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [filter, setFilter] = useState("");
   const [newNumber, setNewNumber] = useState([]);
@@ -98,6 +94,12 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((data) => {
+      setPersons(data.data);
+      console.log(data.data);
+    });
+  }, []);
   return (
     <div>
       <h2>Phonebook</h2>
